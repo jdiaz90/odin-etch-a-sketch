59d45c7342f311260bb04f1f3b96f204fa53486d
Javier Díaz <jdr_1_2_1990@hotmail.com>
2023-01-31 16:43:10 +0100

    Create function to be able to change the number grids

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-function fillContainer() {
+const root = document.querySelector(':root');
+root.style.setProperty('--container-width', '480px');
+let width = getComputedStyle(root).getPropertyValue('--container-width')
 
-    const colorPicker = document.querySelector('#colorPicker')
+start()
+
+function start(){
+
+    const sizeGrid = document.querySelector('#sizePicker')
+    const sizeSelection = document.querySelector('#sizeSelection')
+    let size = 16   
+
+    sizeGrid.min = 1
+    sizeGrid.max = 100
+    sizeGrid.value = size
+    sizeSelection.textContent = `${size} x ${size}`
+
+    sizeGrid.addEventListener('input', (e) => {
+        size = e.target.value
+        sizeSelection.textContent = `${size} x ${size}`
+    })
+    
+    sizeGrid.addEventListener('change', (e) => {
+        size = e.target.value
+        fillContainer(size)
+    })
+
+    fillContainer(size)
+
+}
+
+function fillContainer(size){
+
     const container = document.querySelector('.container')
-    let root = document.querySelector(':root')
+    const colorPicker = document.querySelector('#colorPicker')
     let col, row
 
-    alert(colorPicker.value)
-    
-    for(let i=1; i <= 16; i++){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    for(let i=1; i <= size; i++){
 
         row = document.createElement('div')
         row.classList.add('row')
 
-        for(let i=1; i <= 16; i++){
+        for(let i=1; i <= size; i++){
             col = document.createElement('div')
             col.classList.add('col')
 
@@ -24,10 +56,18 @@ function fillContainer() {
         }
 
         container.append(row)
-        
 
     }
 
+    calculateSize(size)
+
 }
 
-fillContainer()
+function calculateSize(size){
+
+    width = width.replace('px', '')
+    const sizeGrid = (width / size) + 'px'
+    root.style.setProperty('--col-width', sizeGrid);
+    root.style.setProperty('--row-height', sizeGrid);
+
+}
